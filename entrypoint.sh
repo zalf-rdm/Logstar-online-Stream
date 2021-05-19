b56954f2b4db9dfc86f00cc500c999d0e4f2ff44
mwall <mwall@PC-mwall-FDM-Ubuntu20.04>
2021-05-19 11:47:41 +0200

    hot fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# handle debug mode (-v)
 if [[ -v LOGSTAR_DEBUG ]]; then
-    $LOGSTAR_DEBUG="-v"
+    LOGSTAR_DEBUG="-v"
  else
-    $LOGSTAR_DEBUG=""
+    LOGSTAR_DEBUG=""
 fi
 
+# handle logging to file (-log)
 if [[ -v LOGSTAR_LOGFILE ]]; then
-    $LOGSTAR_LOGGING="-log $LOGSTAR_LOGFILE"
+    LOGSTAR_LOGGING="-log $LOGSTAR_LOGFILE"
 else
-    $LOGSTAR_LOGGING=""
+    LOGSTAR_LOGGING=""
 
 python /Logstar-online-Stream/logstar-receiver.py $LOGSTAR_DEBUG $LOGSTAR_LOGGING $LOGSTAR_PARAMS
 
