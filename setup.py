dd4c640611922332c648142168b039ba08cc3611
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2022-09-26 11:50:48 +0200

    added setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
-# TODO
+#!/usr/bin/env python
 
+from setuptools import setup
+
+setup(name='logstar-receiver',
+        version='1.0',
+        description='supporting library for bonares/zalf repository',
+        url='https://github.com/zalf-rdm/Logstar-online-Stream/',
+        author='marcel wallschlaeger',
+        author_email='marcel.wallschlaeger@zalf.de',
+        zip_safe = False,
+        packages=['src','processing_steps'],
+      )
