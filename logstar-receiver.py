56a0f00c0cfd838def443b286b87b9201fc42cee
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2024-07-02 09:02:57 +0200

    minor bugfixes

--- logstar-receiver.py
+++ logstar-receiver.py
@@ -261,9 +261,8 @@ def main():
             )
         try:
             while True:
-                today = datetime.today()
-                tomorrow = today + datetime.datetime.timedelta(days=1)
-                conf["startdate"] = today.strftime("%Y-%m-%d")  # %H:%M:%S
+                today = datetime.datetime.today()
+                conf["startdate"] = datetime.strftime("%Y-%m-%d")  # %H:%M:%S
                 conf["enddate"] = tomorrow.strftime("%Y-%m-%d")
                 logstar.manage_dl_db(
                     conf,
