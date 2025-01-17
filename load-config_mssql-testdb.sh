6fcc5155300faea2c3f9764521a563c5300cb8f3
Marcel Wallschläger <marcel.wallschlaeger@zalf.de>
2025-01-17 13:22:03 +0100

    [Fixes #53] no duplicate handling when write into a database a second time (#54)

--- load-config_mssql-testdb.sh
+++ load-config_mssql-testdb.sh
@@ -11,6 +11,6 @@ export LOGSTAR_STATIONS=""
 # database conf
 export LOGSTAR_DB_USER="SA"
 export LOGSTAR_DB_PASS="MyPassword!"
-export LOGSTAR_DB="logstar"
+export LOGSTAR_DB_DBNAME="logstar"
 export LOGSTAR_DB_DRIVER="ODBC Driver 17 for SQL Server"
 export LOGSTAR_DB_PORT="1433"
