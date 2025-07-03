dd4842c9929d15a1648aa188160a02375ce28ade
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2025-07-03 09:19:36 +0200

    [Fixes #71]: "exit with failure on all errors"

--- logstar_stream/logstar.py
+++ logstar_stream/logstar.py
@@ -213,15 +213,13 @@ def request_data(url, timeout):
 
     """
     logging.debug("requesting {} ...".format(url))
-    try:
-        r = requests.get(url, timeout=timeout)
-    except:
-        return None
+    r = requests.get(url, timeout=timeout)
+
     if r.status_code == 200:
         return r.text
     else:
-        logging.debug("Request error {}".format(r.status_code))
-        return None
+        logging.error("Request error {}".format(r.status_code))
+        exit(1)
 
 
 def download_data(conf, station, timeout=15):
@@ -244,7 +242,7 @@ def download_data(conf, station, timeout=15):
         logging.error(
             f"Error when downloading data for station {station} using url {url}...\n{request}"
         )
-        return None
+        exit(1)
 
 
 def prepare_dataframe(data: Dict, datetime_column: str) -> pd.DataFrame:
@@ -319,6 +317,7 @@ def write_to_database(
             logging.error(
                 f"Table {table_name} has no primary key set on {datetime_column} column, this can result in duplicated data in table  ..."
             )
+            exit(1)
 
     to_sql_arugments = {
         "name": table_name,
@@ -327,18 +326,17 @@ def write_to_database(
         "if_exists": "append",
         "index": False,
         "chunksize": 1024,
-        "method": insert_ignore_conflicts # insert_or_do_nothing_on_conflict,
+        "method": insert_or_do_nothing_on_conflict
     }
 
     try:
         num_rows = len(df)
         logging.info(f"Attempting to insert {num_rows} rows into {table_name} ...")
-        #breakpoint()
         df.to_sql(**to_sql_arugments)
         logging.info(f"succesfully writing data ...")
     except Exception as E:
         logging.error(f"failed writing data: {str(E)[:200]}")  # Print first 200 chars of error
-        
+        exit(1)
 
 def manage_dl_db(
     conf,
